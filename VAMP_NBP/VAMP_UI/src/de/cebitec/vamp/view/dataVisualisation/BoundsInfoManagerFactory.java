0bc5841071b6384d24dd7a2fe17e68b91a49f127
Evgeny Anisiforov evgeny <Evgeny Anisiforov evgeny@cebitec.uni-bielefeld.de>
2013-06-30 16:48:44 +0200

    Dashboard: correct the function of additional viewers (e.g. detailed viewer) not being synchronized with the position of the reference viewer, when using dashboard to open a reference

---
+++ VAMP_NBP/VAMP_UI/src/de/cebitec/vamp/view/dataVisualisation/BoundsInfoManagerFactory.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cebitec.vamp.view.dataVisualisation;
+
+import de.cebitec.centrallookup.CentralLookup;
+import de.cebitec.vamp.databackend.dataObjects.PersistantReference;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * This class keeps track of created BoundsInfoManager-Instances and 
+ * ensures that there is only one BoundsInfoManager per genome
+ * 
+ * This is to ensure, that the panel navigation (i.e. "Jump to") still
+ * works even if there are multiple genome object instances in use
+ * @author Evgeny Anisiforov
+ */
+public class BoundsInfoManagerFactory {
+    private HashMap<Integer, BoundsInfoManager> data;
+    
+    public BoundsInfoManagerFactory() {
+        this.data = new HashMap<Integer, BoundsInfoManager>();
+    }
+    
+    public BoundsInfoManager get(PersistantReference genome) {
+        if (data.containsKey(genome.getId())) return data.get(genome.getId());
+        else {
+            BoundsInfoManager boundsManager = new BoundsInfoManager(genome);
+            data.put(genome.getId(), boundsManager);
+            return boundsManager;
+        }
+    }
+    
+    public void clear() {
+        data.clear();
+    }
+    
+}
