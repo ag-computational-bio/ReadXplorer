52b347d1b8f414bff378cfe807333a5128f193ec
Oliver Schwengers <oliver@schwengers.de>
2014-12-02 14:55:21 +0100

    clean-up before merging version 2.0

--- VAMP_AddOns/VAMP_Thumbnail/src/de/cebitec/vamp/thumbnail/docs/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-@HelpSetRegistration(helpSet = "thumbnail-hs.xml", position = 3308)
-package de.cebitec.vamp.thumbnail.docs;
-
-import org.netbeans.api.javahelp.HelpSetRegistration;
