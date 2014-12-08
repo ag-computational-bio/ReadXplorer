40e5983e1c1eb25ae74d0d21b72586c1af3185d8
Oliver Schwengers <oliver@schwengers.de>
2014-12-08 14:25:22 +0100

    add module UI_DataManagement to core application as part of #3

---
+++ ReadXplorer/ReadXplorer_UI_DataManagement/src/de/cebitec/readXplorer/ui/dataAdministration/DataAdminWizardOverviewPanel.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2014 Institute for Bioinformatics and Systems Biology, University Giessen, Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cebitec.readXplorer.ui.dataAdministration;
+
+
+import de.cebitec.readXplorer.parser.ReferenceJob;
+import de.cebitec.readXplorer.parser.TrackJob;
+import de.cebitec.readXplorer.ui.dataAdministration.actions.DataAdminWizardAction;
+import java.awt.Component;
+import java.util.List;
+import javax.swing.event.ChangeListener;
+import org.openide.WizardDescriptor;
+import org.openide.util.HelpCtx;
+
+
+public class DataAdminWizardOverviewPanel implements
+        WizardDescriptor.Panel<WizardDescriptor> {
+
+    /**
+     * The visual component that displays this panel. If you need to access the
+     * component from this class, just use getComponent().
+     */
+    private OverviewCard component;
+
+
+    // Get the visual component for the panel. In this template, the component
+    // is kept separate. This can be more efficient: if the wizard is created
+    // but never displayed, or not all panels are displayed, it is better to
+    // create only those which really need to be visible.
+    @Override
+    public Component getComponent() {
+        if( component == null ) {
+            component = new OverviewCard();
+        }
+        return component;
+    }
+
+
+    @Override
+    public HelpCtx getHelp() {
+        // Show no Help button for this panel:
+        return HelpCtx.DEFAULT_HELP;
+        // If you have context help:
+        // return new HelpCtx(SampleWizardPanel1.class);
+    }
+
+
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+
+
+    @Override
+    public final void addChangeListener( ChangeListener l ) {
+    }
+
+
+    @Override
+    public final void removeChangeListener( ChangeListener l ) {
+    }
+
+
+    // You can use a settings object to keep track of state. Normally the
+    // settings object will be the WizardDescriptor, so you can use
+    // WizardDescriptor.getProperty & putProperty to store information entered
+    // by the user.
+    @Override
+    @SuppressWarnings( "unchecked" )
+    public void readSettings( WizardDescriptor settings ) {
+        List<ReferenceJob> ref2del = (List<ReferenceJob>) settings.getProperty( DataAdminWizardAction.PROP_REFS2DEL );
+        List<TrackJob> track2del = (List<TrackJob>) settings.getProperty( DataAdminWizardAction.PROP_TRACK2DEL );
+
+        component.showGenereateOverview( ref2del, track2del );
+    }
+
+
+    @Override
+    public void storeSettings( WizardDescriptor settings ) {
+    }
+
+
+}
---
+++ ReadXplorer/ReadXplorer_UI_DataManagement/src/de/cebitec/readXplorer/ui/dataAdministration/actions/DataAdminWizardAction.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2014 Institute for Bioinformatics and Systems Biology, University Giessen, Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.cebitec.readXplorer.ui.dataAdministration.actions;
+
+
+import de.cebitec.centrallookup.CentralLookup;
+import de.cebitec.readXplorer.api.cookies.LoginCookie;
+import de.cebitec.readXplorer.parser.ReferenceJob;
+import de.cebitec.readXplorer.parser.TrackJob;
+import de.cebitec.readXplorer.ui.dataAdministration.DataAdminWizardOverviewPanel;
+import de.cebitec.readXplorer.ui.dataAdministration.DataAdminWizardSelectionPanel;
+import de.cebitec.readXplorer.ui.dataAdministration.DeletionThread;
+import de.cebitec.readXplorer.util.VisualisationUtils;
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.SwingWorker;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.WizardDescriptor;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
+
+
+@ActionID(
+         category = "File",
+         id = "de.cebitec.readXplorer.ui.dataAdministration.actions.DataAdminWizardAction"
+)
+@ActionRegistration(
+         iconBase = "de/cebitec/readXplorer/ui/dataAdministration/manage.png",
+         displayName = "#CTL_DataAdminWizardAction"
+)
+@ActionReferences( {
+    @ActionReference( path = "Menu/File", position = 1462 ),
+    @ActionReference( path = "Toolbars/Management", position = 401 )
+} )
+@NbBundle.Messages( "CTL_DataAdminWizardAction=Manage data" )
+public final class DataAdminWizardAction implements ActionListener {
+
+    private final LoginCookie context;
+    private List<WizardDescriptor.Panel<WizardDescriptor>> panels;
+
+    public static final String PROP_REFS2DEL = "refdel";
+    public static final String PROP_TRACK2DEL = "trackdel";
+
+
+    public DataAdminWizardAction( LoginCookie context ) {
+        this.context = context;
+    }
+
+
+    @Override
+    @SuppressWarnings( "unchecked" )
+    public void actionPerformed( ActionEvent ev ) {
+        if( CentralLookup.getDefault().lookup( SwingWorker.class ) != null ) {
+            NotifyDescriptor nd = new NotifyDescriptor.Message( NbBundle.getMessage( DataAdminWizardAction.class, "MSG_BackgroundActivity" ), NotifyDescriptor.WARNING_MESSAGE );
+            DialogDisplayer.getDefault().notify( nd );
+            return;
+        }
+        if( panels == null ) {
+            panels = new ArrayList<>();
+            panels.add( new DataAdminWizardSelectionPanel() );
+            panels.add( new DataAdminWizardOverviewPanel() );
+        }
+        WizardDescriptor wiz = new WizardDescriptor( new WizardDescriptor.ArrayIterator<>( VisualisationUtils.getWizardPanels( panels ) ) );
+        // {0} will be replaced by WizardDesriptor.Panel.getComponent().getName()
+        wiz.setTitleFormat( new MessageFormat( "{0}" ) );
+        wiz.setTitle( NbBundle.getMessage( DataAdminWizardAction.class, "TTL_DataAdminWizardAction.title" ) );
+        Dialog dialog = DialogDisplayer.getDefault().createDialog( wiz );
+        dialog.setVisible( true );
+        dialog.toFront();
+        boolean cancelled = wiz.getValue() != WizardDescriptor.FINISH_OPTION;
+        if( !cancelled ) {
+            List<ReferenceJob> refs2del = (List<ReferenceJob>) wiz.getProperty( DataAdminWizardAction.PROP_REFS2DEL );
+            List<TrackJob> tracks2del = (List<TrackJob>) wiz.getProperty( DataAdminWizardAction.PROP_TRACK2DEL );
+            DeletionThread dt = new DeletionThread( refs2del, tracks2del );
+            RequestProcessor rp = new RequestProcessor( "Deletion Threads", 2 );
+            rp.post( dt );
+        }
+    }
+
+
+}
