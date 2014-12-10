cdf9126fbc2ae82f63606dabb21c85be347a8b78
Oliver Schwengers <oliver@schwengers.de>
2014-12-10 09:56:42 +0100

    change code base from de.compbio.xxx to de.comp.bio....

--- ReadXplorer_ClassificationUpdate/src/de/comp/bio/readxplorer/classificationUpdate/UpdateReadClassVisualPanel.java
+++ ReadXplorer_ClassificationUpdate/src/de/comp/bio/readxplorer/classificationUpdate/UpdateReadClassVisualPanel.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.compbio.readxplorer.classificationUpdate;
+package de.comp.bio.readxplorer.classificationUpdate;
 
 
 /**
