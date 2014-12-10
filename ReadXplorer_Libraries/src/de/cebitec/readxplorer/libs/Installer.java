1ae3b0aafd6e8e54fe798ae7b7caca5c79bd647a
Oliver Schwengers <oliver@schwengers.de>
2014-12-10 10:40:30 +0100

    change code base in module ReadXplorer_Libs to obey Java conventions and best practices
    de.cebitec.readXplorer -> de.cebitec.readxplorer

--- ReadXplorer_Libraries/src/de/cebitec/readxplorer/libs/Installer.java
+++ ReadXplorer_Libraries/src/de/cebitec/readxplorer/libs/Installer.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.cebitec.readXplorer.libs;
+package de.cebitec.readxplorer.libs;
 
 
 import net.sf.picard.reference.ReferenceSequenceFileFactory;
