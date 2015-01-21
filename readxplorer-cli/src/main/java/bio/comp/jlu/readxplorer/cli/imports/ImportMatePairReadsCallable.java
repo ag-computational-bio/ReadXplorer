332db99d8cb5e4cdcefdede37d11d214dbf6dd47
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2015-01-21 13:32:08 +0100

    introduce ImportMatePairReadsCallable.class

--- readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/imports/ImportMatePairReadsCallable.java
+++ readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/imports/ImportMatePairReadsCallable.java
@@ -18,26 +18,31 @@
 package bio.comp.jlu.readxplorer.cli.imports;
 
 
+import bio.comp.jlu.readxplorer.cli.imports.ImportMatePairReadsCallable.ImportMatePairResults;
+import bio.comp.jlu.readxplorer.cli.imports.ImportReferenceCallable.ImportReferenceResult;
 import de.cebitec.readxplorer.databackend.connector.ProjectConnector;
-import de.cebitec.readxplorer.databackend.connector.StorageException;
-import de.cebitec.readxplorer.databackend.dataObjects.PersistentTrack;
-import de.cebitec.readxplorer.parser.ReferenceJob;
-import de.cebitec.readxplorer.parser.common.ParsedReference;
+import de.cebitec.readxplorer.databackend.dataObjects.PersistentChromosome;
+import de.cebitec.readxplorer.parser.TrackJob;
+import de.cebitec.readxplorer.parser.common.ParsedTrack;
 import de.cebitec.readxplorer.parser.common.ParsingException;
-import de.cebitec.readxplorer.parser.reference.BioJavaGff2Parser;
-import de.cebitec.readxplorer.parser.reference.BioJavaGff3Parser;
-import de.cebitec.readxplorer.parser.reference.BioJavaParser;
-import de.cebitec.readxplorer.parser.reference.FastaReferenceParser;
-import de.cebitec.readxplorer.parser.reference.Filter.FeatureFilter;
-import de.cebitec.readxplorer.parser.reference.Filter.FilterRuleSource;
-import de.cebitec.readxplorer.parser.reference.ReferenceParserI;
-import de.cebitec.readxplorer.utils.Benchmark;
-import java.io.PrintStream;
+import de.cebitec.readxplorer.parser.mappings.JokToBamDirectParser;
+import de.cebitec.readxplorer.parser.mappings.MappingParserI;
+import de.cebitec.readxplorer.parser.mappings.SamBamParser;
+import de.cebitec.readxplorer.parser.mappings.SamBamStatsParser;
+import de.cebitec.readxplorer.utils.GeneralUtils;
+import de.cebitec.readxplorer.utils.StatsContainer;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.netbeans.api.sendopts.CommandException;
-import org.openide.util.NbBundle;
 
 
 /**
@@ -61,101 +66,82 @@ import org.openide.util.NbBundle;
  *
  * @author Oliver Schwengers <oschweng@cebitec.uni-bielefeld.de>
  */
-public final class ImportMatePairReadsCallable implements Callable<PersistentTrack> {
+public final class ImportMatePairReadsCallable implements Callable<ImportMatePairResults> {
 
     private static final Logger LOG = Logger.getLogger( ImportMatePairReadsCallable.class.getName() );
 
-    private final boolean verbosity;
+    private final ImportReferenceResult  referenceResult;
+    private final File trackAFile;
+    private final File trackBFile;
 
 
-    protected ImportMatePairReadsCallable( boolean verbosity ) {
 
-        this.verbosity = verbosity;
+
+    public ImportMatePairReadsCallable( ImportReferenceResult referenceResult, File trackAFile, File trackBFile ) {
+
+        this.referenceResult = referenceResult;
+        this.trackAFile = trackAFile;
+        this.trackBFile = trackBFile;
 
     }
-
-
 
 
 
 
     @Override
-    public PersistentTrack call() throws Exception {
+    public ImportMatePairResults call() throws CommandException {
 
-        PersistentTrack pt;
-
-//        try {
-
-//            final int l = referenceFileArgs.length;
-//            final ReferenceParserI refParser = selectParser( fileTypeArg );
-//            for( int i=0; i<l; i++ ) {
-
-//                ReferenceJob rj = new ReferenceJob( 0, new File(referenceFileArgs[i]), refParser,
-//                    descriptionArgs[i], nameArgs[i], new Timestamp( System.currentTimeMillis() ) );
-//
-//                importRefGenome( rj, ps );
-//
-//            }
-//
-//        }
-//        catch( SQLException ex ) {
-//
-//            CommandException ce = new CommandException( 1 );
-//                ce.initCause( ex );
-//            throw ce;
-//
-//        }
-
-        return null;
-
-    }
-
-
-    /**
-     * Processes reference genome job.
-     */
-    private void importRefGenome( final ReferenceJob rj, final PrintStream ps ) throws CommandException {
-
-        ps.println(NbBundle.getMessage(ImportMatePairReadsCallable.class, "MSG_ImportThread.import.start.ref" ) + ":" );
-        final long start = System.currentTimeMillis();
         try {
 
-            // parse reference genome
-            LOG.log( Level.INFO, "Start parsing reference genome from source \"{0}\"", rj.getFile().getAbsolutePath() );
-            ReferenceParserI parser = rj.getParser();
-            FeatureFilter filter = new FeatureFilter();
-            filter.addBlacklistRule( new FilterRuleSource() );
-            ParsedReference refGenome = parser.parseReference( rj, filter );
-            LOG.log( Level.INFO, "Finished parsing reference genome from source \"{0}\"", rj.getFile().getAbsolutePath() );
-            ps.println("\"" + rj.getName() + "\" " + NbBundle.getMessage(ImportMatePairReadsCallable.class, "MSG_ImportThread.import.parsed" ) );
-
-
-            // stores reference sequence in the db
-            LOG.log( Level.INFO, "Start storing reference genome from source \"{0}\"", rj.getFile().getAbsolutePath() );
-            int refGenID = ProjectConnector.getInstance().addRefGenome( refGenome );
-            rj.setPersistent( refGenID );
-            LOG.log( Level.INFO, "Finished storing reference genome from source \"{0}\"", rj.getFile().getAbsolutePath() );
-
-
-            // print benchmarks
-            if( verbosity ) {
-                final long finish = System.currentTimeMillis();
-                String msg = "\"" + rj.getName() + "\" " + NbBundle.getMessage(ImportMatePairReadsCallable.class, "MSG_ImportThread.import.stored" );
-                ps.println( Benchmark.calculateDuration( start, finish, msg ) );
+            // create necessary (mockup) objects
+            final ImportMatePairResults result = new ImportMatePairResults();
+            final ProjectConnector pc = ProjectConnector.getInstance();
+            final MappingParserI mappingParser = selectParser( trackAFile.getName().substring( trackAFile.getName().lastIndexOf( '.' ) ) );
+            final Map<String, Integer> chromLengthMap = new HashMap<>();
+            Map<Integer, PersistentChromosome> chromIdMap = pc.getRefGenomeConnector( referenceResult.getParsedReference().getID() ).getRefGenome().getChromosomes();
+            for( PersistentChromosome chrom : chromIdMap.values() ) {
+                chromLengthMap.put( chrom.getName(), chrom.getLength() );
             }
+            final StatsContainer statsContainer = new StatsContainer();
+                statsContainer.prepareForTrack();
+            final TrackJob trackJob = new TrackJob( pc.getLatestTrackId(), trackAFile,
+                                              trackAFile.getName(),
+                                              referenceResult.getReferenceJob(), // check if this is ok
+                                              mappingParser,
+                                              false,
+                                              new Timestamp( System.currentTimeMillis() ) );
+
+
+            // parse track file
+            LOG.log( Level.FINE, "parse track file: {0}...", trackAFile.getName() );
+            trackAFile.setReadOnly(); //prevents changes or deletion of original file!
+            //executes any conversion before other calculations, if the parser supports any
+            boolean success = mappingParser.convert( trackJob, chromLengthMap );
+            File lastWorkFile = trackJob.getFile();
+
+            //generate classification data in file sorted by read sequence
+            mappingParser.setStatsContainer( statsContainer );
+            mappingParser.parseInput( trackJob, chromLengthMap );
+            if( success ) {
+                GeneralUtils.deleteOldWorkFile( lastWorkFile );
+            } //only when we reach this line without exceptions and conversion was successful
+            trackAFile.setWritable( true );
+
+            //file needs to be sorted by coordinate for efficient calculation
+            SamBamStatsParser statsParser = new SamBamStatsParser();
+                statsParser.setStatsContainer( statsContainer );
+            ParsedTrack track = statsParser.createTrackStats( trackJob, chromLengthMap );
+            result.setParsedTrack( track );
+
+            LOG.log( Level.FINE, "parsed track file: {0}", trackAFile.getName() );
+            result.addOutput( "parsed track file " + trackAFile.getName() );
+
+            return result;
 
         }
-        catch( ParsingException | StorageException ex ) {
+        catch( ParsingException | IOException | OutOfMemoryError ex ) {
             LOG.log( Level.SEVERE, null, ex );
-//            ps.println( "\"" + rj.getName() + "\" " + NbBundle.getMessage( ImportThread.class, "MSG_ImportThread.import.failed" ) + "!" );
-            CommandException ce = new CommandException( 1, "\"" + rj.getName() + "\" " + NbBundle.getMessage(ImportMatePairReadsCallable.class, "MSG_ImportThread.import.failed" ) );
-                ce.initCause( ex );
-            throw ce;
-        }
-        catch( OutOfMemoryError ex ) {
-            LOG.log( Level.SEVERE, null, ex );
-//            ps.println( "\"" + rj.getName() + "\" " + NbBundle.getMessage( ImportThread.class, "MSG_ImportThread.import.outOfMemory" ) + "!" );
-            CommandException ce = new CommandException( 1, "\"" + rj.getName() + "\" " + NbBundle.getMessage(ImportMatePairReadsCallable.class, "MSG_ImportThread.import.outOfMemory" ) );
+            CommandException ce = new CommandException( 1, "import failed!" );
                 ce.initCause( ex );
             throw ce;
         }
@@ -163,23 +149,47 @@ public final class ImportMatePairReadsCallable implements Callable<PersistentTra
     }
 
 
+    private static MappingParserI selectParser( String fileTypeArg ) {
 
-
-    private static ReferenceParserI selectParser( String fileTypeArg ) {
-
-        switch( fileTypeArg ) {
-            case "embl":
-                return new BioJavaParser( BioJavaParser.EMBL );
-            case "genbank":
-                return new BioJavaParser( BioJavaParser.GENBANK );
-            case "gff3":
-                return new BioJavaGff3Parser();
-            case "gff2":
-            case "gtf":
-                return new BioJavaGff2Parser();
-            case "fasta":
+        switch( fileTypeArg.toLowerCase() ) {
+            case "out":
+            case "jok":
+                return new JokToBamDirectParser();
+            case "sam":
+            case "bam":
             default:
-                return new FastaReferenceParser();
+                return new SamBamParser();
+        }
+
+    }
+
+
+
+
+    public class ImportMatePairResults {
+
+        private final List<String> output;
+        private ParsedTrack pt;
+
+
+        ImportMatePairResults() {
+            this.output = new ArrayList<>( 10 );
+        }
+
+        void addOutput( String msg ) {
+            output.add( msg );
+        }
+
+        public List<String> getOutput() {
+            return Collections.unmodifiableList( output );
+        }
+
+        void setParsedTrack( ParsedTrack pr ) {
+            this.pt = pr;
+        }
+
+        public ParsedTrack getParsedTrack() {
+            return pt;
         }
 
     }
