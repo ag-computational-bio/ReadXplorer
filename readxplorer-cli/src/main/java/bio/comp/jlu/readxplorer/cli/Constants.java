2bad5f5a4216f14d45a78638b203e21067c6e526
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2015-02-16 12:23:03 +0100

    Revert "add paired-end-reads files' pre/in/suffix property"
    This reverts commit 33c1b643c158bbcfa8e4e98f7d5b594eaf2ce0a2.

--- readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/Constants.java
+++ readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/Constants.java
@@ -3,13 +3,10 @@ package bio.comp.jlu.readxplorer.cli;
 
 
 /**
- * Collection of RX cli constants.
+ *
  * @author Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
  */
 public final class Constants {
-
-    // paired-end distance
-    public static final String PER_FILE_DISTINCTION = "per.file.distinction";
 
     // paired-end distance
     public static final String PER_DISTANCE = "per.distance";
