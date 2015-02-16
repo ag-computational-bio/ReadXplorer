33c1b643c158bbcfa8e4e98f7d5b594eaf2ce0a2
Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
2015-02-16 11:36:42 +0100

    add paired-end-reads files' pre/in/suffix property

--- readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/Constants.java
+++ readxplorer-cli/src/main/java/bio/comp/jlu/readxplorer/cli/Constants.java
@@ -3,10 +3,13 @@ package bio.comp.jlu.readxplorer.cli;
 
 
 /**
- *
+ * Collection of RX cli constants.
  * @author Oliver Schwengers <oliver.schwengers@computational.bio.uni-giessen.de>
  */
 public final class Constants {
+
+    // paired-end distance
+    public static final String PER_FILE_DISTINCTION = "per.file.distinction";
 
     // paired-end distance
     public static final String PER_DISTANCE = "per.distance";
