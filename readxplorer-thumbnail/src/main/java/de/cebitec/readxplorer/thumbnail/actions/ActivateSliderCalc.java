47dac2f9741346cbcb46204063fa90c0aba47ff3
Rolf Hilker <rolf.hilker@mikrobio.med.uni-giessen.de>
2015-03-12 01:01:57 +0100

    Fix code styles in Thumbnail module #70

--- readxplorer-thumbnail/src/main/java/de/cebitec/readxplorer/thumbnail/actions/ActivateSliderCalc.java
+++ readxplorer-thumbnail/src/main/java/de/cebitec/readxplorer/thumbnail/actions/ActivateSliderCalc.java
@@ -36,7 +36,7 @@ import org.openide.util.actions.Presenter;
  */
 public final class ActivateSliderCalc implements ActionListener, Presenter.Menu {
 
-    private final String ITEM_TEXT = "Auto Slider Calculation";
+    private static final String ITEM_TEXT = "Auto Slider Calculation";
 
 
     @Override
