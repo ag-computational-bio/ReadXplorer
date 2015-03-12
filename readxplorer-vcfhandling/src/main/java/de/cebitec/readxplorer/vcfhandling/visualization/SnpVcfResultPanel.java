ec81c4dfd5c879d1991eb4bfea6ea54841dd25b8
Rolf Hilker <rolf.hilker@mikrobio.med.uni-giessen.de>
2015-03-12 12:27:51 +0100

    Fix code styles in VCFHandling module #70

--- readxplorer-vcfhandling/src/main/java/de/cebitec/readxplorer/vcfhandling/visualization/SnpVcfResultPanel.java
+++ readxplorer-vcfhandling/src/main/java/de/cebitec/readxplorer/vcfhandling/visualization/SnpVcfResultPanel.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Institute for Bioinformatics and Systems Biology, University Giessen, Germany
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package de.cebitec.readxplorer.vcfhandling.visualization;
@@ -140,8 +151,7 @@ public class SnpVcfResultPanel extends ResultTablePanel {
                 vcfStatsMap.put( FILTERS, 0 );
                 vcfStatsMap.put( ATTRIBUTES, 0 );
                 vcfData.setStatsMap( vcfStatsMap );
-            }
-            else {
+            } else {
                 this.completeVcfData.getSnpVcfList().addAll( vcfData.getSnpVcfList() );
             }
             final int snpVcfDataSize = 10;
