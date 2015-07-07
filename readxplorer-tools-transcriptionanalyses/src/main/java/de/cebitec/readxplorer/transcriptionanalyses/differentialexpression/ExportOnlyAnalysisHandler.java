7acb39f2acb2d14fe899db8e84827fbf55923e01
Kai Stadermann <kstaderm@cebitec.uni-bielefeld.de>
2015-07-07 13:14:09 +0200

    ExportOnlyAnalysisHandler now shows Locus of Gene

--- readxplorer-tools-transcriptionanalyses/src/main/java/de/cebitec/readxplorer/transcriptionanalyses/differentialexpression/ExportOnlyAnalysisHandler.java
+++ readxplorer-tools-transcriptionanalyses/src/main/java/de/cebitec/readxplorer/transcriptionanalyses/differentialexpression/ExportOnlyAnalysisHandler.java
@@ -72,37 +72,34 @@ public class ExportOnlyAnalysisHandler extends DeAnalysisHandler {
         List<List<Object>> tableContents = new ArrayList<>();
 
         final ReferenceConnector referenceConnector = getReferenceConnector();
-        //This offset must correspond to the additional fields added by hand
-        final int offset = 8;
         for( i = 0; i < data.getFeatures().length; i++ ) {
 
             boolean allZero = true;
-            final Object[] tmp = new Object[data.getSelectedTracks().size() + offset];
-            /*
-             * Here the additional fields are added. If one field is added or
-             * remove the "offset" value must be changed accordingly.
-             */
-            tmp[0] = referenceConnector.getChromosomeForGenome( feature[i].getChromId() );
+            final List<Object> tmp = new ArrayList<>();
+            // Here the additional fields are added. If something is added don't
+            // forget to also enter a additional colum name further down.
+            tmp.add( feature[i].getLocus() );
+            tmp.add( referenceConnector.getChromosomeForGenome( feature[i].getChromId() ) );
             if( feature[i].isFwdStrand() ) {
-                tmp[1] = "fw";
+                tmp.add( "fw" );
             } else {
-                tmp[1] = "rv";
+                tmp.add( "rv" );
             }
-            tmp[2] = feature[i].getStart();
-            tmp[3] = feature[i].getStop();
-            tmp[4] = calculateFeatureTypeLength( feature[i], FeatureType.EXON );
-            tmp[5] = calculateFeatureTypeLength( feature[i], FeatureType.INTRON );
-            tmp[6] = feature[i].getLength();
-            tmp[7] = feature[i].getType();
-            for( int j = offset; j < data.getSelectedTracks().size() + offset; j++ ) {
-                int value = countData[j - offset][i];
+            tmp.add( feature[i].getStart() );
+            tmp.add( feature[i].getStop() );
+            tmp.add( calculateFeatureTypeLength( feature[i], FeatureType.EXON ) );
+            tmp.add( calculateFeatureTypeLength( feature[i], FeatureType.INTRON ) );
+            tmp.add( feature[i].getLength() );
+            tmp.add( feature[i].getType() );
+            for( int j = 0; j < data.getSelectedTracks().size(); j++ ) {
+                int value = countData[j][i];
                 if( value != 0 ) {
                     allZero = false;
                 }
-                tmp[j] = value;
+                tmp.add( value );
             }
             if( !allZero ) {
-                tableContents.add( new ArrayList<>( Arrays.asList( tmp ) ) );
+                tableContents.add( tmp );
                 regionNamesList.add( feature[i] );
             }
             progressHandle.progress( i );
@@ -110,6 +107,7 @@ public class ExportOnlyAnalysisHandler extends DeAnalysisHandler {
 
         String[] trackDescriptions = data.getTrackDescriptions();
         List<Object> colNames = new ArrayList<>( trackDescriptions.length + 10 );
+        colNames.add( "Locus" );
         colNames.add( "Chromosome" );
         colNames.add( "Strand" );
         colNames.add( "Start" );
