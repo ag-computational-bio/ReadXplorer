a86634b6b0643a47581f0235e42fa8b5c849e259
Rolf Hilker <rolf.hilker@mikrobio.med.uni-giessen.de>
2015-12-04 09:39:19 +0100

    #143 & #144 Fix finish button enabled in main wizard page and add storing of last parameter selection for correlation analysis

--- readxplorer-rnatrimming/src/main/java/de/cebitec/readxplorer/rnatrimming/correlationanalysis/ParameterSelectionPanel.java
+++ readxplorer-rnatrimming/src/main/java/de/cebitec/readxplorer/rnatrimming/correlationanalysis/ParameterSelectionPanel.java
@@ -18,54 +18,37 @@
 package de.cebitec.readxplorer.rnatrimming.correlationanalysis;
 
 
+import de.cebitec.readxplorer.ui.dialogmenus.ChangeListeningWizardPanel;
 import java.awt.Component;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import org.openide.WizardDescriptor;
-import org.openide.util.HelpCtx;
+
+import static de.cebitec.readxplorer.rnatrimming.correlationanalysis.CorrelationAnalysisAction.PROP_CORRELATIONCOEFFICIENT;
+import static de.cebitec.readxplorer.rnatrimming.correlationanalysis.CorrelationAnalysisAction.PROP_INTERVALLENGTH;
+import static de.cebitec.readxplorer.rnatrimming.correlationanalysis.CorrelationAnalysisAction.PROP_MINCORRELATION;
+import static de.cebitec.readxplorer.rnatrimming.correlationanalysis.CorrelationAnalysisAction.PROP_MINPEAKCOVERAGE;
 
 
 /**
- * Panel for the parameter selection as part of the correlation analysis wizard
+ * Panel for the parameter selection as part of the correlation analysis wizard.
  * <p>
- * @author Evgeny Anisiforov <evgeny at cebitec.uni-bielefeld.de>
+ * @author Evgeny Anisiforov <evgeny at cebitec.uni-bielefeld.de>, rhilker
  */
-class ParameterSelectionPanel implements
-        WizardDescriptor.FinishablePanel<WizardDescriptor> {
-
-    ParameterSelectionPanel() {
-        PropertyChangeListener pc = new PropertyChangeListener() {
-            @Override
-            public void propertyChange( PropertyChangeEvent evt ) {
-                propertyChanged();
-            }
-
-
-        };
-
-        this.getComponent().addPropertyChangeListener( CorrelationAnalysisAction.PROP_INTERVALLENGTH, pc );
-        this.getComponent().addPropertyChangeListener( CorrelationAnalysisAction.PROP_MINCORRELATION, pc );
-    }
-
-
-    private void propertyChanged() {
-        isValid = (component.getIntervalLength() > 2) && (component.getMinimumCorrelation() > 0);
-        fireChangeEvent();
-    }
-
+class ParameterSelectionPanel extends ChangeListeningWizardPanel {
 
     /**
      * The visual component that displays this panel. If you need to access the
      * component from this class, just use getComponent().
      */
     private ParameterSelectionCard component;
-    private boolean isValid;
-    private final Set<ChangeListener> listeners = new HashSet<>( 1 ); // or can use ChangeSupport in NB 6.0
+
+
+    /**
+     * Panel for the parameter selection as part of the correlation analysis
+     * wizard.
+     */
+    ParameterSelectionPanel() {
+        super( "Interval length needs to be > 2 and minimum correlcation > 0" );
+    }
 
 
     // Get the visual component for the panel. In this template, the component
@@ -82,66 +65,24 @@ class ParameterSelectionPanel implements
 
 
     @Override
-    public HelpCtx getHelp() {
-        // Show no Help button for this panel:
-        return HelpCtx.DEFAULT_HELP;
-        // If you have context help:
-        // return new HelpCtx(SampleWizardPanel1.class);
-    }
-
-
-    @Override
-    public boolean isValid() {
-        return isValid;
-    }
-
-
-    @Override
-    public boolean isFinishPanel() {
-        return isValid;
-    }
-
-
-    @Override
-    public final void addChangeListener( ChangeListener l ) {
-        synchronized( listeners ) {
-            listeners.add( l );
-        }
-    }
-
-
-    @Override
-    public final void removeChangeListener( ChangeListener l ) {
-        synchronized( listeners ) {
-            listeners.remove( l );
-        }
-    }
-
-
-    protected final void fireChangeEvent() {
-        Iterator<ChangeListener> it;
-        synchronized( listeners ) {
-            it = new HashSet<>( listeners ).iterator();
-        }
-        ChangeEvent ev = new ChangeEvent( this );
-        while( it.hasNext() ) {
-            it.next().stateChanged( ev );
-        }
-    }
-
-
-    @Override
-    public void readSettings( WizardDescriptor data ) {
-
-    }
-
-
-    @Override
     public void storeSettings( WizardDescriptor settings ) {
-        settings.putProperty( CorrelationAnalysisAction.PROP_INTERVALLENGTH, component.getIntervalLength() );
-        settings.putProperty( CorrelationAnalysisAction.PROP_MINCORRELATION, component.getMinimumCorrelation() );
-        settings.putProperty( CorrelationAnalysisAction.PROP_MINPEAKCOVERAGE, component.getMinimumPeakCoverage() );
-        settings.putProperty( CorrelationAnalysisAction.PROP_CORRELATIONCOEFFICIENT, component.getCorrelationMethod() );
+        settings.putProperty( PROP_INTERVALLENGTH, component.getIntervalLength() );
+        settings.putProperty( PROP_MINCORRELATION, component.getMinimumCorrelation() );
+        settings.putProperty( PROP_MINPEAKCOVERAGE, component.getMinimumPeakCoverage() );
+        settings.putProperty( PROP_CORRELATIONCOEFFICIENT, component.getCorrelationMethod() );
+        storePrefs();
+    }
+
+
+    /**
+     * Stores the chosen correlation analysis parameters for this wizard for
+     * later use, also after restarting the software.
+     */
+    private void storePrefs() {
+        getPref().putInt( PROP_INTERVALLENGTH, component.getIntervalLength() );
+        getPref().putInt( PROP_MINCORRELATION, component.getMinimumCorrelation() );
+        getPref().putInt( PROP_MINPEAKCOVERAGE, component.getMinimumPeakCoverage() );
+        getPref().put( PROP_CORRELATIONCOEFFICIENT, component.getCorrelationMethod().toString() );
     }
 
 
